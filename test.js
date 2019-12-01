026a3ee7d57428f67be058c148fb0575d96dd7f9
Tambourine <ImaginaryBrother@users.noreply.github.com>
2019-12-01 22:53:49 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 const now_date = new Date();
 const luciano_release_date = new Date("September 4, 2019");
 
-var month = now_date.getmMonth();
-var month2 = luciano_release_date.getMonth()
+var month = now_date..getMonth();
