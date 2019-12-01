064e7505544d48f20c5121edc26de1bacfee51a1
Tambourine <ImaginaryBrother@users.noreply.github.com>
2019-12-01 23:13:30 +0900

    2 files update

--- test.js
+++ test.js
@@ -1,4 +1,16 @@
-const now_date = new Date();
-const luciano_release_date = new Date("September 4, 2019");
+const now = new Date();
+const luciano_release = new Date("September 4, 2019");
 
-var month = now_date.getMonth() - luciano_release_date.getMonth();
+const now_year = now.getFullYear();
+const now_month = now.getMonth();
+const now_date = now.getDate();
+
+const release_year = luciano_release.getFullYear();
+const release_month = luciano_release.getMonth();
+const release_date = luciano_release.getDate();
+
+if(now_year == release_year){
+  var duration_month = now_date.getMonth() - luciano_release_date.getMonth();
+}else{
+  var duration_month = "未実装です！"
+}
